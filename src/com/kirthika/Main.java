cdada0668e0f573edca7e25970603c0555338df6
Kirthika S <kirthika05@gmail.com>
2017-01-26 20:48:31 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,39 @@
+package com.kirthika;
+
+import java.util.*;
+
+//Write your code here
+class Calculator{
+    int power(int n,int p) throws Exception{
+        if( n<0 || p<0 ){
+            throw new Exception("n and p should be non-negative");
+        }
+        return (int) Math.pow(n,p);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        int T=in.nextInt();
+        while(T-->0)
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try
+            {
+                int ans=myCalculator.power(n,p);
+                System.out.println(ans);
+
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
